5e0cef1c861e1474303300880110c1050ce1927f
scbedd <45376673+scbedd@users.noreply.github.com>
2022-02-14 13:25:17 -0800

    adding a shortcut script that will test exactly what the other python build is invoking

---
+++ scripts/devops_tasks/test_proxy_python_startup.py
@@ -0,0 +1,74 @@
+import requests
+import time
+import os
+import shlex
+import subprocess
+
+CONTAINER_NAME = "ambitious_azsdk_test_proxy"
+LINUX_IMAGE_SOURCE_PREFIX = "azsdkengsys.azurecr.io/engsys/testproxy-lin"
+WINDOWS_IMAGE_SOURCE_PREFIX = "azsdkengsys.azurecr.io/engsys/testproxy-win"
+CONTAINER_STARTUP_TIMEOUT = 6000
+PROXY_MANUALLY_STARTED = os.getenv("PROXY_MANUAL_START", False)
+
+PROXY_URL = os.getenv("PROXY_URL", "https://localhost:5001")
+REPO_ROOT = os.path.abspath(os.path.join(os.path.abspath(__file__), "..", "..", "..", ".."))
+PROXY_CHECK_URL = PROXY_URL.rstrip("/") + "/Info/Available"
+TOOL_ENV_VAR = "PROXY_PID"
+
+
+def check_availability():
+    # type: () -> None
+    """Attempts request to /Info/Available. If a test-proxy instance is responding, we should get a response."""
+    try:
+        response = requests.get(PROXY_CHECK_URL, timeout=60)
+        return response.status_code
+    # We get an SSLError if the container is started but the endpoint isn't available yet
+    except requests.exceptions.SSLError as sslError:
+        return response.status_code
+    except Exception as e:
+        print(e)
+        return response.status_code
+
+
+def check_proxy_availability():
+    # type: () -> None
+    """Waits for the availability of the test-proxy."""
+    start = time.time()
+    now = time.time()
+    status_code = 0
+    while now - start < CONTAINER_STARTUP_TIMEOUT and status_code != 200:
+        status_code = check_availability()
+        now = time.time()
+
+
+def start_test_proxy():
+    # type: () -> None
+    """Starts the test proxy and returns when the proxy server is ready to receive requests. In regular use
+    cases, this will auto-start the test-proxy docker container. In CI, or when environment variable TF_BUILD is set, this
+    function will start the test-proxy .NET tool."""
+
+    if not PROXY_MANUALLY_STARTED:
+        if os.getenv("TF_BUILD"):
+            print("Starting the test proxy tool...")
+            if check_availability() == 200:
+                print("Tool is responding, exiting...")
+            else:
+                envname = os.getenv("TOX_ENV_NAME", "default")
+                log = open("_proxy_log_{}.log".format(envname), "a")
+                proc = subprocess.Popen(
+                    shlex.split('test-proxy --storage-location="{}" --urls {}'.format(REPO_ROOT, PROXY_URL)),
+                    stdout=log,
+                    stderr=log,
+                )
+                os.environ[TOOL_ENV_VAR] = str(proc.pid)
+        else:
+            print("Starting the test proxy container...")
+
+            proc = subprocess.Popen(shlex.split("docker container start " + CONTAINER_NAME))
+            proc.communicate()
+
+        # Wait for the proxy server to become available
+        check_proxy_availability()
+
+if __name__ == "__main__":
+    start_test_proxy()
