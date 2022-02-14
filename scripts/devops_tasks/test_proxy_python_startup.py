1ac1af0b5543954d895355d3050d3f4647ef45d9
scbedd <45376673+scbedd@users.noreply.github.com>
2022-02-14 14:29:20 -0800

    fix exception

--- scripts/devops_tasks/test_proxy_python_startup.py
+++ scripts/devops_tasks/test_proxy_python_startup.py
@@ -24,10 +24,10 @@ def check_availability():
         return response.status_code
     # We get an SSLError if the container is started but the endpoint isn't available yet
     except requests.exceptions.SSLError as sslError:
-        return response.status_code
+        return 404
     except Exception as e:
         print(e)
-        return response.status_code
+        return 404
 
 
 def check_proxy_availability():
